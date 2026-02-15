8719865e61f41d0af46f16a45afae5ab5bb3c8b1
dj044 <daljeet1279@gmail.com>
2026-02-15 09:32:11 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/lean90/", // <-- change to your repo name (e.g., "/lean90/")
+  base: "/lean90/", // EXACT repo name, case-sensitive
 });
